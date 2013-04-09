1386910231235dde0b8a910a0ba1f5a144f6b486
Seb Ashton <seb@madebymade.co.uk>
2013-04-09 16:50:57 +0100

    extra check on validator

--- app/validators/promotion.rb
+++ app/validators/promotion.rb
@@ -2,7 +2,7 @@ module Promotion
   module PromotionValidators
     class PlacementLimitValidator < ActiveModel::EachValidator
       def validate_each(record, attribute, value)
-        return if value.nil? or !record.new_record?
+        return if value.nil? or !record.new_record? or !Promotion.config.placements[value].has_key?(:max)
 
         if Promotion::Promo.count(:conditions => {:placement => value}) >= Promotion.config.placements[value][:max]
           record.errors[attribute] << "The maximum number of entries for this placement has already been reached"
